e9d590264707ef643b68586736e323dd1ab02337
xielei999 <329444310@qq.com>
2016-05-01 23:26:43 +0800

    Stage to local lib

---
+++ Test_Sln/Test_Console/Test_Console.cpp
@@ -0,0 +1,15 @@
+// Test_Console.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"Test Fun"<<endl;
+	int n;
+	cin>>n;
+	return 0;
+}
+
